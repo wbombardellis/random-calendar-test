e8df8a982b5c7d4943355d3a8e8549ad3e5a12d6
William Bombardelli da Silva <wbombardellis@gmail.com>
2024-10-27 22:04:04 -0300

    Add log4j and selenium

--- src/test/kotlin/tests/CalendarTests.kt
+++ src/test/kotlin/tests/CalendarTests.kt
@@ -1,12 +1,30 @@
 package tests
 
+import org.apache.logging.log4j.LogManager
 import org.junit.jupiter.api.Test
+import org.openqa.selenium.firefox.FirefoxDriver
 
+import org.openqa.selenium.firefox.FirefoxOptions
+
+import org.openqa.selenium.remote.RemoteWebDriver
+import java.net.URL
+
+
+private const val WEBDRIVER_URL_CONFIG = "org.wbombardellis.plexus.webdriver"
 
 class CalendarTests {
 
+    private val logger = LogManager.getLogger()
+
     @Test
     fun testAmountOfDates() {
-
+        val remoteWebDriverURL = System.getProperty(WEBDRIVER_URL_CONFIG)
+        logger.info("Remote web driver URL: $remoteWebDriverURL")
+        val driver = if (remoteWebDriverURL != null) {
+            RemoteWebDriver(URL(remoteWebDriverURL), FirefoxOptions())
+        } else {
+            FirefoxDriver()
+        }
+        driver.quit()
     }
 }
