718c90d012dd79700c4f2a36b7c8741f1dbba922
William Bombardelli da Silva <wbombardellis@gmail.com>
2024-10-27 22:20:37 -0300

    Organize basic test structure

---
+++ src/test/kotlin/common/Constants.kt
@@ -0,0 +1,4 @@
+package common
+
+const val RANDOM_CALENDAR_URL = "https://www.random.org/calendar-dates/"
+const val WEBDRIVER_URL_CONFIG = "org.wbombardellis.plexus.webdriver"
--- src/test/kotlin/tests/CalendarTests.kt
+++ src/test/kotlin/tests/CalendarTests.kt
@@ -1,30 +1,37 @@
 package tests
 
+import common.RANDOM_CALENDAR_URL
+import common.WEBDRIVER_URL_CONFIG
 import org.apache.logging.log4j.LogManager
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.firefox.FirefoxDriver
-
 import org.openqa.selenium.firefox.FirefoxOptions
-
 import org.openqa.selenium.remote.RemoteWebDriver
 import java.net.URL
 
-
-private const val WEBDRIVER_URL_CONFIG = "org.wbombardellis.plexus.webdriver"
 
 class CalendarTests {
 
     private val logger = LogManager.getLogger()
 
-    @Test
-    fun testAmountOfDates() {
+    private val driver by lazy {
         val remoteWebDriverURL = System.getProperty(WEBDRIVER_URL_CONFIG)
         logger.info("Remote web driver URL: $remoteWebDriverURL")
-        val driver = if (remoteWebDriverURL != null) {
-            RemoteWebDriver(URL(remoteWebDriverURL), FirefoxOptions())
-        } else {
-            FirefoxDriver()
+        runCatching {
+            if (remoteWebDriverURL != null) {
+                RemoteWebDriver(URL(remoteWebDriverURL), FirefoxOptions())
+            } else {
+                FirefoxDriver()
+            }
+        }.getOrElse {
+            logger.fatal("Could not initialize web driver. Check your configuration.")
+            throw it
         }
+    }
+
+    @Test
+    fun testAmountOfDates() {
+        driver.get(RANDOM_CALENDAR_URL)
         driver.quit()
     }
 }
